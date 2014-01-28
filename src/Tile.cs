e6e313ad5c7f39d920866af9df96f8dcfe5768e6
ZEP-Arter <zyborgedproductions@gmail.com>
2014-01-28 15:43:44 -0500

    Created Things + Updated Tile
    Small Update.
    Things class created with preliminary variables
    Tiles class updated with preliminary variables

--- src/Tile.cs
+++ src/Tile.cs
@@ -1,7 +1,17 @@
 namespace GameLogic
 {
-	public class Tile
-	{
-		
-  }
+    //THERE ARE 48 TILES
+    //NUMBER OF TILES ON A 4 PLAYER BOARD IS 37
+    public class Tile
+    {
+        //Type of Hex: Jungle, Frozen Waste, Desert, Plains, Forest, Sea, Swamp, Mountain
+        private string hexType;
+        //Shows whether the Tile is face up or face down
+        private bool faceUp;
+        //Shows whether the Tile can be selected as a starting point
+        private bool startPossible;
+
+        private int playerControl;
+
+    }
 }
