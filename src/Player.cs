55f45bd309dcaed3d45eb9902577007ff39c2115
Eric <er.adamski@gmail.com>
2014-01-28 11:13:53 -0500

    file structure, change if you like

--- src/Player.cs
+++ src/Player.cs
@@ -6,11 +6,19 @@ namespace GameLogic
 		
 		//public
 		
+		public string getName()
+		{ return name; }
+		
+		public void setName(string n)
+		{ name = n; }
+		
 		//private
 		
 		//private members
 		//
 		//	name
+			private string name;
 		//	
+		//  
 	}
 }
