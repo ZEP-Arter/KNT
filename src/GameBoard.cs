2a0aa6e7820d5ddb4f128a73b240e4b8196f0f47
Eric <er.adamski@gmail.com>
2014-01-29 11:53:42 -0500

    Small Update
    Small change to game idea, having a singleton to keep track of the
    entire game which can be held by the host and be used to set phases,
    and player turns.

---
+++ src/GameBoard.cs
@@ -0,0 +1,41 @@
+
+namespace GameLogic
+{
+
+	class GameBoard
+	{
+		
+		// @ericadamski : I think this should be a singleton.
+		
+		//public
+		
+			public getGame() { return game; }
+		
+			public createGame() { game = new GameBoard(); }
+		
+		//private
+		
+			//ctor
+			private GameBoard () {} 
+		
+		// private members
+		
+			// singleton Access
+			private GameBoard game;
+		
+			// List of Players
+			//
+		
+			// Board Obj
+			//
+		
+			// the Bank
+			//
+		
+			// the Playing cup
+			//
+		
+		
+	} // end GameBorad Class
+
+} // end namespace
--- src/Player.cs
+++ src/Player.cs
@@ -6,11 +6,11 @@ namespace GameLogic
 		
 		//public
 		
-		public string getName()
-		{ return name; }
+			public string getName()
+			{ return name; }
 		
-		public void setName(string n)
-		{ name = n; }
+			public void setName(string n)
+			{ name = n; }
 		
 		//private
 		
@@ -19,6 +19,11 @@ namespace GameLogic
 		//	name
 			private string name;
 		//	
-		//  
+		//  things ( this will have to have a bool (isOnRack)
+		//
+		//  characters ( for all non-special Characters this will also have to have a bool (isOnRacK)
+		//
+		// Rack ( this is an Obj, with just a list. mostly logic )
+		// ( cannot keep gold counters, special characters, and forts )
 	}
 }
