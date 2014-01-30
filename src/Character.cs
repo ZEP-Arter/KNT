7dca80553fd2c18c42538474c1dec175db9ce9c5
Eric <er.adamski@gmail.com>
2014-01-30 13:39:35 -0500

    update
    Updated character class

--- src/Character.cs
+++ src/Character.cs
@@ -1,11 +1,48 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using KNT.GUI;
+
 namespace GameLogic
 {
-    class Character : IImage
+    abstract class SpecialCharacter : IImage
     {
+		//ctor
+			public SpecialCharacter( string n, List<string> ab )
+			{
+				name = n;
+				
+				abilities = ab;
+				
+				isOwned = false;
+			}
+		//public
+		
+			public bool getIsOwned()
+			{ return isOwned; }
+			
+			public string getName()
+			{ return name; }
+			
+			public void setOwned()
+			{ isOwned ? (isOwned = false) : (isOwned = true) ; }
+		
+		//private
+		
+		//protected
+			protected setName(string n)
+			{ name = n; }
+		
+		//private members
+		
+			// special abilities
+			private List<string> abilities; // maybe make this a class in itself ( 'abilities' ) not sure yet as to what the contain
+			
+			// character name/type
+			private string name;
+			
+			//is owned by a player ( not in pool next to the bank)
+			private bool isOwned;
     }
 }
