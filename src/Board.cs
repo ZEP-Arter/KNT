cfd64810fa28aec1824049ecfd4f5b5f6dc2f1cd
Eric <er.adamski@gmail.com>
2014-01-30 08:39:06 -0500

    Merge branch 'iteration_one' of https://github.com/ZEP-Arter/KNT into iteration_one
    Conflicts:
    	src/Board.cs

--- src/Board.cs
+++ src/Board.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 namespace GameLogic
 {
 
@@ -15,3 +16,30 @@ namespace GameLogic
 	} // class
 
 } // namspace
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameLogic
+{
+    class Board
+    {
+        //ctor 
+
+        public Board() 
+        {
+            tiles = new List<Tile>();
+        }
+
+        //public
+
+        //private
+
+        //private members
+
+            List<Tile> tiles;
+    }
+}
+>>>>>>> effce3281d248b5a8d3b456484b2c7ffd09cb18f
---
+++ src/Things.cs
@@ -0,0 +1,24 @@
+namespace GameLogic
+{
+    //THERE ARE 351 THINGS
+    public class Things
+    {
+        //Type of Thing:    Special Income, Creature, Magic, Treasure, Event, Gold Counter,
+        //                  Fort, Control Marker, Special Character
+        string type;
+        string name;
+        string hexType; //if applicable
+        int combatValue; //if applicable
+
+        bool inBank;
+        bool onRack;
+
+        int goldValue; //if applicable
+
+        string attribute; //if applicable
+
+        
+
+
+    }
+}
