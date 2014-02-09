60c6fe60294bbcd1a5fc398a753caa5df2baa4dc
ZEP-Arter <zyborgedproductions@gmail.com>
2014-02-09 02:17:15 -0500

    Update Tile.cs
    includes all the variables needed for Movement and most for Combat

--- src/Tile.cs
+++ src/Tile.cs
@@ -4,6 +4,36 @@ namespace GameLogic
     //NUMBER OF TILES ON A 4 PLAYER BOARD IS 37
     public class Tile
     {
+        //Hex Number
+        int hexNumber;
+        
+        //Number for connected hexes
+        private int nHex;
+        private int neHex;
+        private int seHex;
+        private int sHex;
+        private int swHex;
+        private int neHex;
+        //Type of Hex: Jungle, Frozen Waste, Desert, Plains, Forest, Sea, Swamp, Mountain
+        private string hexType;
+        //Shows whether the Tile is face up or face down
+        private bool faceUp = false;
+        //Shows whether the Tile can be selected as a starting point
+        private bool startPossible;
+        //Holds stacks of things for each player
+        public List<Thing> p1Stack;
+        public List<Thing> p2Stack;
+        public List<Thing> p3Stack;
+        public List<Thing> p4Stack;
+        
+        private bool rough = false;
+
+		private Player playerControl;
+        
+        //Movement logic variable
+        public bool traversed = false;
+        
+        //Constructor
         public Tile(int n, int[6] a, string h)
         {
             hexNumber   = n;
@@ -14,25 +44,30 @@ namespace GameLogic
             swHex       = a[4];
             neHex       = a[5];
             hexType     = h;
+            if(h == "Swamp" || h == "Forest" || h == "Mountain" || h == "Jungle")
+                rough = true;
         }
-        //Hex Number
-        int hexNumber;
         
-        //Number for connected hexes
-        int nHex;
-        int neHex;
-        int seHex;
-        int sHex;
-        int swHex;
-        int neHex;
-        //Type of Hex: Jungle, Frozen Waste, Desert, Plains, Forest, Sea, Swamp, Mountain
-        private string hexType;
-        //Shows whether the Tile is face up or face down
-        private bool faceUp;
-        //Shows whether the Tile can be selected as a starting point
-        private bool startPossible;
-
-		private Player playerControl;
+        public Tile(int n, int[6] a, string h, bool s)
+        {
+            hexNumber   = n;
+            nHex        = a[0];
+            neHex       = a[1];
+            seHex       = a[2];
+            sHex        = a[3];
+            swHex       = a[4];
+            neHex       = a[5];
+            hexType     = h;
+            if(h == "Swamp" || h == "Forest" || h == "Mountain" || h == "Jungle")
+                rough = true;
+            startPossible = s;
+        }
+        
+        public void resetMovementLogic() { traversed = false; }
+        public void isRough() { return rough; }
+        public void getType() { return hexType; }
+        public void getStart() { return startPossible; }
+        public void flipTile() { faceUp = true; }
 
     }
 }
